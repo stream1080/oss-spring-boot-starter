ded76757241e0e486e5bfc746ab3d03471a2a028
stream1080 <fmxhax638@foxmail.com>
2023-03-02 17:18:53 +0800

    feat: 配置文件增加自定义域名和资源访问方式

--- src/main/java/com/stream1080/oss/OssProperties.java
+++ src/main/java/com/stream1080/oss/OssProperties.java
@@ -29,6 +29,19 @@ public class OssProperties {
     private String endpoint;
 
     /**
+     * 自定义域名
+     */
+    private String customDomain;
+
+    /**
+     * 资源访问方式
+     * <p>
+     * true path-style nginx 反向代理和 S3 默认支持 pathStyle {http://endpoint/bucketname}
+     * false supports virtual-hosted-style 阿里云等需要配置为 virtual-hosted-style 模式 {http://bucketname.endpoint}
+     */
+    private Boolean pathStyleAccess = true;
+
+    /**
      * 区域
      */
     private String region;
